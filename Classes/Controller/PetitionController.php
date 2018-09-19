9255b911ba95bd0094441a674bc1aee4add4744c
Ubuntu <ubuntu@ip-172-31-24-232.eu-central-1.compute.internal>
2018-09-19 10:47:41 +0000

    test

--- Classes/Controller/PetitionController.php
+++ Classes/Controller/PetitionController.php
@@ -1,112 +1,120 @@
 <?php
+
 namespace Dnilabs\DnilabsPetition\Controller;
+
 use \TYPO3\CMS\Core\Utility\GeneralUtility as GU;
 use \Dnilabs\DnilabsPetition\Domain\Model\Participant;
-class PetitionController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
 
-  /**
-   * @var \Dnilabs\DnilabsPetition\Service\EmailService
-   * @inject
-   */
-  protected $emailService = NULL;
+class PetitionController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+{
 
-  /**
-   * @var \Dnilabs\DnilabsPetition\Domain\Repository\ParticipantRepository
-   * @inject
-   */
-  protected $participantRepository = NULL;
+    /**
+     * @var \Dnilabs\DnilabsPetition\Service\EmailService
+     * @inject
+     */
+    protected $emailService = NULL;
 
-  /**
-   * @var \Dnilabs\DnilabsPetition\Domain\Repository\PetitionRepository
-   * @inject
-   */
-  protected $petitionRepository = NULL;
+    /**
+     * @var \Dnilabs\DnilabsPetition\Domain\Repository\ParticipantRepository
+     * @inject
+     */
+    protected $participantRepository = NULL;
 
-  /**
-   * @var \TYPO3\CMS\Extbase\Domain\Repository\FrontendUserGroupRepository
-   * @inject
-   */
-  protected $frontendUserGroupRepository;
+    /**
+     * @var \Dnilabs\DnilabsPetition\Domain\Repository\PetitionRepository
+     * @inject
+     */
+    protected $petitionRepository = NULL;
 
-  /**
-   * @return void
-   */
-  public function showAction() {
-    $uid = $this->settings["petition"];
-    $this->view->assign('petition', $this->petitionRepository->findByUid($uid));
-    $this->view->assign('new', new Participant);
-  }
+    /**
+     * @var \TYPO3\CMS\Extbase\Domain\Repository\FrontendUserGroupRepository
+     * @inject
+     */
+    protected $frontendUserGroupRepository;
 
-  /**
-   * @return void
-   */
-  public function activationAction() {
-    $token = GU::_GET('uniquehash');
-    if (!$token) return "kein token";
-    $user = $this->participantRepository->findOneByUsername($token);
-    /* echo $user->getUsername(); */
-    if (!$user) return "token nicht gefunden";
-    if (!$user->getDisable()) return "bereits aktiviert";
-    $this->view->assign("participant", $user);
-    $user->setDisable(false);
-    $this->participantRepository->update($user);
-  }
-
-  /**
-   * @return void
-   */
-  public function listAction() {
-    $page = intval(GU::_GET('page'));
-    if ($page<=0) {
-      $page = 1;
+    /**
+     * @return void
+     */
+    public function showAction()
+    {
+        $uid = $this->settings["petition"];
+        $this->view->assign('petition', $this->petitionRepository->findByUid($uid));
+        $this->view->assign('new', new Participant);
     }
-    $pet = $this->settings["petition"];
-    $limit = $this->settings["limit"];
-    $pets = $this->petitionRepository->findPage($page, $limit, $pet);
-    $this->view->assign('petitionen', $pets);
-  }
 
-  /**
-   * @param \Dnilabs\DnilabsPetition\Domain\Model\Participant $new
-   * @return void
-   */
-  public function createAction(\Dnilabs\DnilabsPetition\Domain\Model\Participant $new) {
+    /**
+     * @return void
+     */
+    public function activationAction()
+    {
+        $token = GU::_GET('uniquehash');
+        if (!$token) return "kein token";
+        $user = $this->participantRepository->findOneByUsername($token);
+        /* echo $user->getUsername(); */
+        if (!$user) return "token nicht gefunden";
+        if (!$user->getDisable()) return "bereits aktiviert";
+        $this->view->assign("participant", $user);
+        $user->setDisable(false);
+        $this->participantRepository->update($user);
+    }
 
-    $fegroup = $this->settings["fegroup"];
-    $usergroup = $this->frontendUserGroupRepository->findByUid($fegroup);
-    if (!$usergroup) return "no usergroup";
+    /**
+     * @return void
+     */
+    public function listAction()
+    {
+        $page = intval(GU::_GET('page'));
+        if ($page <= 0) {
+            $page = 1;
+        }
+        $pet = $this->settings["petition"];
+        $limit = $this->settings["limit"];
+        $pets = $this->participantRepositoryRepository->findPage($page, $limit, $pet);
+        $this->view->assign('petitionen', $pets);
+    }
 
-    // set attributes
-    $new->addUsergroup($usergroup);
-    $new->setDisable(true);
-    $new->setDate(new \Datetime);
-    $new->setUsername(md5(time()));
-    $new->setPassword("8oHb^2qBjE/z),+L8oHb^2qBjE/z),+L");
+    /**
+     * @param \Dnilabs\DnilabsPetition\Domain\Model\Participant $new
+     * @return void
+     */
+    public function createAction(\Dnilabs\DnilabsPetition\Domain\Model\Participant $new)
+    {
 
-    // count up
-    $pet =  $new->getPetition();
-    $nr = $pet->getCount() + 1;
-    $pet->setCount($nr);
-    $new->setNumber($nr);
+        $fegroup = $this->settings["fegroup"];
+        $usergroup = $this->frontendUserGroupRepository->findByUid($fegroup);
+        if (!$usergroup) return "no usergroup";
 
-    // save
-    $this->participantRepository->add($new);
-    $this->petitionRepository->update($pet);
+        // set attributes
+        $new->addUsergroup($usergroup);
+        $new->setDisable(true);
+        $new->setDate(new \Datetime);
+        $new->setUsername(md5(time()));
+        $new->setPassword("8oHb^2qBjE/z),+L8oHb^2qBjE/z),+L");
 
-    // send activation email
-    $to = [$new->getEmail()];
-    $from = [$this->settings["emailfrom"]];
-    $subject = $this->settings["emailsubject"];
-    $this->emailService->sendTemplateEmail($to, $from, $subject, "Activation", [
-      "confirmurl" => $this->settings["baseurl"],
-      "confirmpid" => $this->settings["success2"],
-      "token" => $new->getUsername()
-    ]);
+        // count up
+        $pet = $new->getPetition();
+        $nr = $pet->getCount() + 1;
+        $pet->setCount($nr);
+        $new->setNumber($nr);
 
-    // redirect to success1
-    $pageUid = $this->settings['success1'];
-    $uri = $this->uriBuilder->setTargetPageUid($pageUid)->build();
-    $this->redirectToURI($uri);
+        // save
+        $this->participantRepository->add($new);
+        $this->petitionRepository->update($pet);
 
-  }
+        // send activation email
+        $to = [$new->getEmail()];
+        $from = [$this->settings["emailfrom"]];
+        $subject = $this->settings["emailsubject"];
+        $this->emailService->sendTemplateEmail($to, $from, $subject, "Activation", [
+            "confirmurl" => $this->settings["baseurl"],
+            "confirmpid" => $this->settings["success2"],
+            "token"      => $new->getUsername()
+        ]);
+
+        // redirect to success1
+        $pageUid = $this->settings['success1'];
+        $uri = $this->uriBuilder->setTargetPageUid($pageUid)->build();
+        $this->redirectToURI($uri);
+
+    }
 }
--- Classes/Domain/Repository/ParticipantRepository.php
+++ Classes/Domain/Repository/ParticipantRepository.php
@@ -1,28 +1,33 @@
 <?php
+
 namespace Dnilabs\DnilabsPetition\Domain\Repository;
-class ParticipantRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
+class ParticipantRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
+{
 
-  protected $defaultOrderings = array(
-    'number' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_DESCENDING
-  );
+    protected $defaultOrderings = array(
+        'number' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_DESCENDING
+    );
 
-  public function findOneByUsername($name) {
-    $query = $this->createQuery();
-    $query->matching(
-      $query->equals("username", $name)
-    );
-    return $query->setLimit(1)->execute()->getFirst();
-  }
-  public function findPage($page, $limit, $pet) {
-    $query = $this->createQuery();
-    $query->matching(
-      $query->equals("petition", $pet)
-    );
-    if ($page > 1) {
-      $offset = $page * $limit;
-      $query->setOffset($offset);
+    public function findOneByUsername($name)
+    {
+        $query = $this->createQuery();
+        $query->matching(
+            $query->equals("username", $name)
+        );
+        return $query->setLimit(1)->execute()->getFirst();
     }
-    return $query->setLimit($limit)->execute();
-  }
+
+    public function findPage($page, $limit, $pet)
+    {
+        $query = $this->createQuery();
+        $query->matching(
+            $query->equals("petition", $pet)
+        );
+        if ($page > 1) {
+            $offset = $page * $limit;
+            $query->setOffset($offset);
+        }
+        return $query->setLimit($limit)->execute();
+    }
 
 }
